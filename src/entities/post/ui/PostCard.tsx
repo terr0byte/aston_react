e87194d5ab370bd56eda340d8611e08d0d0f8258
terr0byte <il.fa201114@yandex.ru>
2026-01-05 02:21:54 +0300

    feat: add router, navtab

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -2,26 +2,23 @@ import { useState } from "react";
 import { useTheme } from "../../../shared/lib/theme/useTheme"
 import Button from "../../../shared/ui/Button/Button";
 import { CommentListWithLoading } from "../../../widgets/CommentList/ui/CommentList";
-
-export type Post = {
-    userId: number,
-    id: number,
-    title: string,
-    body: string,
-}
+import type { Post } from "../../../utils/types";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export function PostCard({ title, body, id }: Post) {
     const { theme } = useTheme();
     const [isOpen, setOpen] = useState(false);
+    let navigate = useNavigate();
 
     function handleClick() {
         setOpen(!isOpen)
+        isOpen ? navigate('/posts') : navigate(`${id}/comments`); //переделать в отдельный компонент позже
     }
-
+    // кнопку переделать в отдельный компонент позже
     return <div data-component='card' data-theme={theme}>
         <h2>{title}</h2>
         <p>{body}</p>
-        <Button onClick={handleClick}>Открыть комментарии</Button>
-        {isOpen && <CommentListWithLoading postId={id} />}
+        <Button onClick={handleClick}>Открыть комментарии</Button> 
+        {isOpen && <CommentListWithLoading postId={id} />} 
     </div>
 }
---
+++ src/pages/Photos/Photos.tsx
@@ -0,0 +1,3 @@
+export function Photos() {
+    return (<div>Фото</div>)
+}
